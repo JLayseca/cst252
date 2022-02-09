26b8785e6852b286421d3fbd77c32aac16881e25
JLayseca <98487425+JLayseca@users.noreply.github.com>
2022-02-08 23:36:29 -0800

    Update lab.js

---
+++ lab4/js/lab.js
@@ -0,0 +1,42 @@
+// Summary: Pseudocode comments for the computer's behavior as the user orders someting from Amzaon.
+// Description: Pseudocode converted to comments to make theoretically written code more readable to any human being examining the code. (use period)
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author Jeremy Layseca, Sugi Sugihara.
+// @since  2.8.2022
+
+// Task: Buying a flash drive off of Amazon
+           //Turn the computer on.
+           //Request log in.<br>
+           //Log in when passcode is entered.<br>
+           //If the passcode is entered incorrectly, request that it be reentered.
+        //Open Chrome.
+           //Connect to the internet.
+           //Load Chrome's homepage.
+           //Load the user's personal information (frequently visited).
+        //Search for Amazon in the search bar.
+           //Navitate to and load Amazon's homepage.
+        //Log into Amazon.
+           //Retrieve the profile infomaton of the user.
+        //Search for "flash drive".
+           //Retrieve all items labeled under "flash drive".
+           //Check for any filters applied by the user and present products based
+           //Load results based on browsing history and products related to the search.
+        //Click on a flash drive.
+           //Retrieve the information for the flash drive.<br>
+           //Load pictures, the description, specs, reccomended pairings, simmilaritems, shopping trends, page ads, customer Q&As, videos of related products,and the purchase bar.
+        //Click on "Add to cart".
+           //Add the flash drive to the user's car.<br>
+           //Load new page indicating that the flash drive has been added to the cart.
+           //Load related searches and suggested addons.
+        //Click on "Proceed to checkout".
+           //Retreive the user's stored shipping address/es.
+           //Retrieve the user's payment method/s.
+           //Calculate the shipping based on the user's chosen address.
+           //Load the user's order summary.
+        //Click on "Place your order".
+           //Send the order request to the respective flash drive's company.
+           //Send an order confirmation email to the user's email.
+           //Generate the estimated arrival date.
+           //If the user does not have Prime, then load the ad for a Prime free trial.
+           //If the user does not have the Amazon Visa card, load and ad for the Amazon Visa card.
