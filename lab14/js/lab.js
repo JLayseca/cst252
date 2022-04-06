447e300b70e1b08d47dd3865274313d1aad47ca4
JLayseca <98487425+JLayseca@users.noreply.github.com>
2022-04-05 21:57:27 -0700

    Update lab.js

--- lab14/js/lab.js
+++ lab14/js/lab.js
@@ -1,59 +1,32 @@
 /*
- * Author: Jeremy
+ * Author: Sugi and Jeremy
  * Created: 15th March
  * License: Public Domain
  */
 
-//reference HTML variables
-const quizContainer = document.getElementByIdyId("quiz");
-const resultsContainer = document.getElementById("results");
-const submitButton = document.getElementById("submit");
+//Create a function.
 
-//Function that builds quiz
-function quizBuilder(){
 
+for(var i = 1; i < 200, i++){
+
+  if(i % 3 == 0){
+
+  }
+  if(i % 5 == 0){
+
+  }
+  if(i % 7 == 0){
+
+  }
 }
 
-//Function that displays results
-function resultDisplay(){
-
-}
-
-//Calls function to display the quiz immediately
-quizBuilder();
-
-//Runs the result displaying function when the button is pressed
-submitButton.addEventListener("click", resultDisplay);
-
-const questions = [
-  {
-    question: "What percent of plastic is recycled?",
-    answers: {
-      a: "52%",
-      b: "24%",
-      c: "9%",
-      d: "3%"
-    },
-    rightAnswer: "c"
-  },
-  {
-    question: "How many tons of plastic are estimated to be in the ocean?",
-    answers: {
-      a: "52%",
-      b: "24%",
-      c: "9%",
-      d: "3%"
-    },
-    rightAnswer: "c"
-  },
-  {
-    question: "What percent of plastic is recycled?",
-    answers: {
-      a: "52%",
-      b: "24%",
-      c: "9%",
-      d: "3%"
-    },
-    rightAnswer: "c"
-  },
-]
+var mybutton = document.getElementById("button");
+button.addEventListener("click", function(){
+  //get the value from the input field
+  var name = document.getElementById("input").value;
+  // use the name to get house from SortingHat function
+  var houseObj = sortingHat(name);
+  //output to the output div
+  newText = "The Sorting Hat has sorted you into " + houseStr;
+  document.getElementById("output").innerHTML = newText;
+})
